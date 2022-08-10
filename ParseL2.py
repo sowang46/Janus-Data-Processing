499818389e550ad2f23c113b34d5264b08c043f3
Song Wang <songwang@Songs-MacBook-Air-2022.local>
2022-08-10 19:30:31 -0400

    Add l2 visualization script

---
+++ ParseL2.py
@@ -0,0 +1,46 @@
+import os 
+import argparse
+import seaborn as sns
+import matplotlib.pyplot as plt
+from Utilities.preprocessing import read_to_pandas
+
+def isNaN(item):
+    return item!=item
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description='Parse L2 data from Janus debugger data')
+    parser.add_argument('--data_path', type=str, default='', help='The path to the L2 data file')
+    parser.add_argument('--verbose', type=bool, default=False, help='Print debug info')
+    parser.add_argument('--relative_time', type=bool, default=False, help='Show relative timestamp starting from 0')
+    args = parser.parse_args()
+
+    data = read_to_pandas(args.data_path)
+    assert 'boReport' in data.keys(), "Input file is not L2 data"
+    if args.verbose:
+        for k, v in data.items():
+            print(f"\nData type: {k}")
+            print(v)
+    if args.relative_time:
+        t_min = min([min(data[ft]['Timestamp']) for ft in data.keys()])
+        for ft in data.keys():
+            data[ft]['Timestamp'] = [item-t_min for item in data[ft]['Timestamp'].tolist()]
+    for ft in data.keys():
+        data[ft]['Timestamp(ms)'] = [item/1e6 for item in data[ft]['Timestamp'].tolist()]
+        data[ft]['Timestamp(s)'] = [item/1e3 for item in data[ft]['Timestamp(ms)'].tolist()]
+
+    # Visualize
+    # Convert all list in 'bsrNet' coloum to int 
+    data['bsrReport']['bsrNet'] = [item[0] if not isNaN(item) else item
+                                    for item in data['bsrReport']['bsrNet'].tolist()]
+    plt.figure()
+    sns.set_theme(style="whitegrid")
+    sns.lineplot(data=data['bsrReport'], x='Timestamp(s)', y='bsrNet')
+
+    data['boReport']['queueLoad'] = [item[0] if not isNaN(item) else item
+                                    for item in data['boReport']['queueLoad'].tolist()]
+    plt.figure()
+    sns.lineplot(data=data['boReport'], x='Timestamp(s)', y='queueLoad')
+
+    plt.figure()
+    sns.lineplot(data=data['csiReport'], x='Timestamp(s)', y='cqi')
+    plt.show()
--- Utilities/preprocessing.py
+++ Utilities/preprocessing.py
@@ -1,6 +1,18 @@
-import json
+import json, ast
+import pandas as pd
 from operator import itemgetter
 
+
+'''
+    Janus data info: the key is the frame type name;
+    The first tuple is the field names for frame and slot,
+    The second is for frame data
+'''
+frame_types = {'boReport': (('sfn', 'slot'), ("rnti", "lcId", "queueLoad")), 
+               'bsrReport': (('sfn', 'slot'), ("rnti", "lcgBitmask", "bsrNet")), 
+               'csiReport': (('sfn', 'slot'), ("rnti", "pmiI11", "pmiI12", "pmiI13", "pmiI2", "pmiIdx", "ri", "cqi")),
+               'ulSchPdu': (('frame', 'slot'), ("rnti", "mcs", "nLayers", "nAntennaP", "nPmi", "raType", "rbStart", "rbSize", "nTbSize", "rv", "ndi", "nRetx", "nHarqid")),
+               'dlSchPdu': (('frame', 'slot'), ("rnti", "raType", "rbStart", "rbSize", "nRbg", "nRbgSize", "mcs", "rv", "ndi", "nLayers", "nAntennaP", "nTbSize", "nRetx", "nHarqid"))}
 
 def convert(data_fn):
     '''
@@ -22,7 +34,7 @@ def convert(data_fn):
             data.append((stream_id, stream_sn, payload))
         data = sorted(data, key=itemgetter(1))
     # Write to JSON
-    with open(f'tmp_json', 'a') as f:
+    with open(f'tmp_json', 'w') as f:
         for item in data:
             payload_str = str(item[2])
             payload_str = payload_str.replace("\"", "\\\"")
@@ -31,5 +43,51 @@ def convert(data_fn):
                               "\"stream_sn\": ", str(item[1]), ", "
                               "\"stream_payload\": \"", payload_str, "\"}]\n"]))
 
-if __name__=="__main__":
-    convert('./../data/0809/0809_l2_codelets_test_1.txt')
+def parse_line(line):
+    '''
+    Read one line of JSON file
+
+    '''
+    json_file = json.loads(line.strip())
+    data={}
+    for temp in json_file:
+        if 'stream_payload' in temp:
+            stream_sn = int(temp["stream_sn"])
+            temp = ast.literal_eval(temp['stream_payload'])
+            for ft in frame_types.keys():
+                if ft in temp:
+                    frame_type = ft
+            if 'frame_type' not in locals():
+                raise KeyError('Message type not recognized')
+            data = temp[frame_type][0]
+            timestamp = temp['timestamp']
+            sfn = temp[frame_types[frame_type][0][0]]
+            slot = temp[frame_types[frame_type][0][1]]
+    return frame_type, data, timestamp, sfn, slot
+
+def read_to_pandas(data_fn):
+    '''
+    Read Janus debugger data from a file and return a list of pandas dataframes
+    
+    '''
+    convert(data_fn)
+    with open(f'tmp_json', 'r') as f:
+        lines = f.readlines()
+        data_p = {}
+        for line in lines:
+            frame_type, data, t, sfn, slot = parse_line(line)
+            if frame_type not in data_p.keys():
+                data_p[frame_type]={field:[] for field in frame_types[frame_type][1]}
+                data_p[frame_type]['Timestamp']=[]
+                data_p[frame_type]['sfn']=[]
+                data_p[frame_type]['slot']=[]
+            data_p[frame_type]['Timestamp'].append(int(t))
+            data_p[frame_type]['sfn'].append(int(sfn))
+            data_p[frame_type]['slot'].append(int(slot))
+            for field in frame_types[frame_type][1]:
+                if field in data.keys():
+                    data_p[frame_type][field].append(data[field])
+                else:
+                    data_p[frame_type][field].append(float('nan'))
+    data_p = {ft:pd.DataFrame(data_p[ft]) for ft in data_p.keys()}
+    return data_p
